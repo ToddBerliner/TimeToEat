6fe98c22be74b9016650df206048f7e338ed146e
Todd Berliner <todd@altrooz.com>
2017-09-28 20:27:47 -0700

    Prettier

--- Tech/react-app-created/src/store/reducer.js
+++ Tech/react-app-created/src/store/reducer.js
@@ -1,29 +1,36 @@
-import { combineReducers } from 'redux';
-import { getDateKey, getDayIdsBetweenDayIds, getDow,
-    createDayAndNodes } from '../utils';
-import uiState from './uiState/reducer';
-import plan, { getPlanDayByDayId } from './plan/reducer';
-import days, { getDayById, getDayKeys } from './days/reducer';
-import nodes from './nodes/reducer';
+import { combineReducers } from "redux";
+import {
+    getDateKey,
+    getDayIdsBetweenDayIds,
+    getDow,
+    createDayAndNodes
+} from "../utils";
+import uiState from "./uiState/reducer";
+import plan, { getPlanDayByDayId } from "./plan/reducer";
+import days, { getDayById, getDayKeys } from "./days/reducer";
+import nodes from "./nodes/reducer";
 
 // Action Types
-export const DAY_AND_NODES_ADDED = 'day_added';
+export const DAY_AND_NODES_ADDED = "day_added";
 
 // Actions
-export const _ensureDaysAndNodes = (sinceDayId, throughDayId = getDateKey()) => {
+export const _ensureDaysAndNodes = (
+    sinceDayId,
+    throughDayId = getDateKey()
+) => {
     const dayIdsToCreate = getDayIdsBetweenDayIds(sinceDayId, throughDayId);
     if (dayIdsToCreate.length > 0) {
         return function(dispatch, getState) {
             const dayAndNodesPayloads = [];
             // map over dayIdsToCreate, create the payload and dispatch the action
-            dayIdsToCreate.map((dayId) => {
+            for (var dayId of dayIdsToCreate) {
                 const planDay = getPlanDayByDayId(getState().plan, dayId);
                 const dayAndNodes = createDayAndNodes(dayId, planDay);
                 dispatch({ type: DAY_AND_NODES_ADDED, dayAndNodes });
-            });
-        }
+            }
+        };
     }
-}
+};
 
 // Reducers
 const store = combineReducers({
@@ -37,4 +44,4 @@ export default store;
 // Selectors
 export const _getDayById = (state, dayId) => {
     return getDayById(state.days, dayId);
-}
+};
