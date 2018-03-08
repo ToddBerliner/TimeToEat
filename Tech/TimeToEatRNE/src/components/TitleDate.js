f1f91f3784fd872caa4ecffd3d850f8ee080bd4b
Todd Berliner <todd@altrooz.com>
2018-03-08 09:06:05 -0800

    Removed debug code

--- Tech/TimeToEatRNE/src/components/TitleDate.js
+++ Tech/TimeToEatRNE/src/components/TitleDate.js
@@ -3,7 +3,6 @@ import { Text, StyleSheet } from "react-native";
 import { months, getDow, getFriendlyDate } from "../utils";
 
 const TitleDate = props => {
-  console.log(props.dayId);
   const dateStr = getFriendlyDate(props.dayId);
   return <Text style={styles.titleDate}>{dateStr}</Text>;
 };
